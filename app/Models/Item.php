1e9a05d1792be03818a9a5ce203050ab869aacb3
ArindaRodrick <arinda.rodrick@gmail.com>
2023-11-10 11:23:22 +0300

    Updated Item and User Models and added Priority and Category models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+    protected $with = ['category', 'author'];
 public function scopeSortByNameAsc($query)
 {
     return $query->orderBy('name', 'asc');
@@ -19,6 +20,28 @@ public function scopeSortByNameAsc($query)
                 $query->where('name', 'like', '%' . $search . '%')
             )
         );
-}
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+        $query->whereHas('category', fn ($query) =>
+            $query->where('slug', $category)
+        )
+    );
 
+    $query->when($filters['author'] ?? false, fn($query, $author) =>
+        $query->whereHas('author', fn ($query) =>
+            $query->where('username', $author)
+        )
+    );
+}
+public function category()
+{
+    return $this->belongsTo(Category::class);
+}
+public function author()
+{
+    return $this->belongsTo(User::class, 'user_id');
+}
+public function priority()
+{
+    return $this->belongsTo(Priority::class);
+}
 }
