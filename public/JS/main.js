c9f58f86470ba1741581faca7520dfd1dec8203b
ArindaRodrick <arinda.rodrick@gmail.com>
2023-11-10 18:55:59 +0300

    Updated the public file with css fileand js file

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,4 +1,12 @@
-function confirmDelete() 
+function confirmDeleteItem() 
 {
     return confirm('Are you sure you want to delete the item');   
+}
+function confirmDeleteCategory() 
+{
+    return confirm('Are you sure you want to delete the Category');   
+}
+function confirmDeletePriority() 
+{
+    return confirm('Are you sure you want to delete the Priority');   
 }
