23a1e4499333a4904dd70fb15f2f5128f7dd478d
ArindaRodrick <arinda.rodrick@gmail.com>
2023-11-26 10:59:10 +0300

    updated the routes with password reset and newpassword routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
-use App\Http\Controllers\NewPasswordController;
-use App\Http\Controllers\PasswordResetLinkController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\ItemController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PriorityController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', [ItemController::class, 'index'])->name('home');
-Route::get('items/create', [ItemController::class, 'create']);
-Route::post('items', [ItemController::class, 'store']);
-Route::get('items/{item}/edit', [ItemController::class, 'edit']);
-Route::patch('items/{item}', [ItemController::class, 'update']);
-Route::delete('items/{item}', [ItemController::class, 'destroy']);
+
 
 
 Route::middleware('guest')->group(function () {
@@ -45,18 +37,29 @@ Route::post('login', [SessionsController::class, 'store']);
 });
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
-
+Route::middleware('auth')->group(function () {
+    //ITEMS
+    
+    Route::get('/', [ItemController::class, 'index'])->name('home');
+    Route::get('items/create', [ItemController::class, 'create']);
+    Route::post('items', [ItemController::class, 'store']);
+    Route::get('items/{item}/edit', [ItemController::class, 'edit']);
+    Route::patch('items/{item}', [ItemController::class, 'update']);
+    Route::delete('items/{item}', [ItemController::class, 'destroy']);
+    //CATEGORIES
 Route::get('categories', [CategoryController::class, 'index']);
 Route::get('categories/create', [CategoryController::class, 'create']);
 Route::post('categories', [CategoryController::class, 'store']);
 Route::get('categories/{category}/edit', [CategoryController::class, 'edit']);
 Route::patch('categories/{category}', [CategoryController::class, 'update']);
 Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
-
+//PRIORITIES
 Route::get('priorities', [PriorityController::class, 'index']);
 Route::get('priorities/create', [PriorityController::class, 'create']);
 Route::post('priorities', [PriorityController::class, 'store']);
 Route::get('priorities/{priority}/edit', [PriorityController::class, 'edit']);
 Route::patch('priorities/{priority}', [PriorityController::class, 'update']);
 Route::delete('priorities/{priority}', [PriorityController::class, 'destroy']);
+});
+
 
